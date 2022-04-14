0ccbe71c8b63979e46ac69cf7991ea98925fbe04
Carlos Sanchez <sanchezcau@gmail.com>
2022-04-14 17:57:32 -0400

    primeras modificaciones

---
+++ src/app/ingreso.ts
@@ -0,0 +1,12 @@
+export interface Ingreso {
+    id: number;
+    fecha: string,
+    rut: number;
+    name: string;
+    boleta: number;
+    producto: number;
+    desc: string;
+    resumen: string;
+    estado: string;
+    tlf: number;
+  }
